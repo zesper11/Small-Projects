222e2bc75298fc59f31bc99f8a4b528f070e2a7c
Rohan <zesper46@gmail.com>
2025-01-20 08:15:59 +0545

    Update script.js

--- Small-Projects/countdown-clock/script.js
+++ Small-Projects/countdown-clock/script.js
@@ -26,7 +26,7 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const timerBox = document.querySelectorAll('.deadline-format h4');
 
-let giveawayDate = new Date(2025,4,24,10,3,0);
+let giveawayDate = new Date(2026,0,1,1,1,0);
 console.log(giveawayDate);
 
 let year = giveawayDate.getFullYear();
@@ -50,6 +50,28 @@ function getRemainingTime(){
   let presentTime = new Date().getTime();
    let remainingTime = futureTime - presentTime;
    console.log(remainingTime);
+   //times in miliseconds
+   const oneDay = 24*60*60*1000;
+   const oneHour = 60*60*1000;
+   const oneMin = 60*1000;
+
+   let daysRemains = Math.floor(remainingTime/oneDay);
+   console.log(daysRemains);
+   let hourRemains = Math.floor((remainingTime % oneDay) / oneHour);
+   console.log(hourRemains);
+   let minRemains = Math.floor((remainingTime % oneHour) / oneMin);
+   console.log(minRemains);
+   let secRemains = Math.floor((remainingTime % oneMin) / 1000);
+   console.log(secRemains);
+
+   //array for the time remaining values
+   let timeRemainsValue = [daysRemains,hourRemains,minRemains,secRemains];
+
+   timerBox.forEach(function(timerBox,index){
+    timerBox.innerHTML = timeRemainsValue[index];
+   });
+
 }
 
-getRemainingTime()
+let countDown = setInterval(getRemainingTime,1000);
+getRemainingTime();
